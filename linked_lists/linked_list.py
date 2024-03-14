27e0d2349cbc9bdea97e1ae4a4d777a7b00fcd2a
csilvaz <carlos_sz7@protonmail.com>
2024-03-13 20:23:41 -0700

    Add find() function to LinkedList

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -1,31 +1,53 @@
+## Linked List Implementation
+
+## Node Class:
+## - Data
+## - Reference to next node
 class Node:
-    def __init__(self, data) -> None:
+    def __init__(self, data) -> None: ## Type hint is 'None'
         self.data = data
         self.next = None
 
+
+## LinkedList Class
+## - Head: Reference to first node
+## - Method: Insert at beginning
+## - Method: Print to python list
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
 
-    # O(1): Constant time
+    ## Insertion (at beginning)
+    ## Constant time operation O(1)
     def insert_at_beginning(self, data):
-        # Create Node
+        # Create new node
         new_node = Node(data)
 
-        # Is the head an empty node?
-        if self.head is None:
-            # If so, then have the head point to the new node
+        # Check if head is already pointing to a node
+        if self.head is None: #i.e. if head is not pointing to anything
+            # Just point head to the new node
             self.head = new_node
             return
-        else: # If head is already pointing to another node
-            new_node.next = self.head   # point the new node to the head pointer (i.e. the current head)
-            self.head = new_node        # point the head to the new node
-    
-    # for testing purposes
+        else: # Otherwise...head IS pointing to another node
+            ## 1. Point our new_node to the current first node
+            new_node.next = self.head
+            ## 2. Point the head to the new node
+            self.head = new_node
+
+
     def to_list(self):
-        elements = []
+        elements = [] ## Create an empty python list (AKA dynamic array)
         current = self.head
         while current:
             elements.append(current.data)
             current = current.next
         return elements
+    
+    def find(self, target):
+        curr = self.head
+        while curr:
+            if target == curr.data: ## found the value
+                return curr
+            curr = curr.next
+        return None 
+
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -5,3 +5,31 @@ def test_insert_into_empty_list():
     list.insert_at_beginning(1)
 
     assert list.to_list() == [1]
+
+def test_insert_into_nonempty_list():
+    list = LinkedList()
+    list.insert_at_beginning(1)
+    list.insert_at_beginning(2)
+    list.insert_at_beginning(3)
+    assert list.to_list() == [3, 2, 1]
+
+def test_find():
+    list = LinkedList()
+    list.insert_at_beginning(1)
+    list.insert_at_beginning(2)
+    list.insert_at_beginning(3)
+    
+    target = 2
+    found_node = list.find(target)
+    
+    ## Verification
+    assert found_node is not None ## Check object is not null
+    assert found_node.data == target ## Check that the value is actually correct
+
+    ## Edge Case: Value is not found
+    missing_target = 4
+    assert list.find(4) is None
+
+    ## Edge Case: Searching an empty list
+    empty_list = LinkedList()
+    assert empty_list.find(target) is None
